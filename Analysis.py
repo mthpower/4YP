56c74125b679769c35fb4d6dcbd8ec9d8222ef19
Matthew Power <mth.power@gmail.com>
2012-10-24 21:57:28 +0100

    Starting the repo

---
+++ Analysis.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  3body.py
+#
+#  Copyright 2012 Matthew <matthew@matthew-VirtualBox>
+#-----------------------------------------------------------------------------
+#imports
+
+
+import pylab
+import math
